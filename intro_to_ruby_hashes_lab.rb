8108ed2185cc1de6f1451f16cd77fa3380264f6d
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 19:05:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,10 @@ def monopoly_with_fourth_tier
 				{
 					"mortgage_value" => "$100"
 				},
-				:pennsylvania_railroad => {},
+				:pennsylvania_railroad =>
+				{
+					"mortgage_value" => "2100"
+				},
 				:b_and_o_railroad => {},
 				:shortline => {},
 			},
